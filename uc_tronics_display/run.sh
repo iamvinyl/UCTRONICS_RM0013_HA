7b2130537c876ddf5b257dce1a108943ef4d4703
iamVinyl <53709201+iamvinyl@users.noreply.github.com>
2026-07-22 20:03:12 -0400

    Update run.sh

--- uc_tronics_display/run.sh
+++ uc_tronics_display/run.sh
@@ -1,17 +1,49 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Start!"
+bashio::log.info "Starting UCTRONICS RM0004 display"
 
-if ls /dev/i2c-1; then 
-    echo "Found i2c access!";
-    echo "Loading C script for UCTRONICS OLED...";
-
-    cd /lcd_display/
-    # make clean
-    make 
-
-    echo "UCTRONICS OLED Display now be showing information";
-    ./display
-else
-echo "no found i2c!"
+if [[ ! -e /dev/i2c-1 ]]; then
+    bashio::log.fatal "I2C device /dev/i2c-1 was not found."
+    exit 1
 fi
+
+cd /lcd_display/ || exit 1
+
+bashio::log.info "Building the LCD display program"
+make clean
+make
+
+display_args=()
+
+if bashio::config.true 'show_home_assistant_logo'; then
+    display_args+=("--logo")
+fi
+
+if bashio::config.true 'show_ip_address'; then
+    display_args+=("--ip")
+fi
+
+if bashio::config.true 'show_cpu_usage'; then
+    display_args+=("--cpu")
+fi
+
+if bashio::config.true 'show_ram_usage'; then
+    display_args+=("--ram")
+fi
+
+if bashio::config.true 'show_disk_space'; then
+    display_args+=("--disk")
+fi
+
+screen_duration="$(bashio::config 'screen_duration')"
+
+if [[ ${#display_args[@]} -eq 0 ]]; then
+    bashio::log.warning \
+        "No display screens are enabled. Falling back to the Home Assistant logo."
+    display_args+=("--logo")
+fi
+
+bashio::log.info "Enabled display arguments: ${display_args[*]}"
+bashio::log.info "Screen duration: ${screen_duration} seconds"
+
+exec ./display "${display_args[@]}" --duration "${screen_duration}"
